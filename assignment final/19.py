a7cf36f6a283100aef18b1d2c87bc40243a5b21e
archana-9430 <archanakumari943064@gmail.com>
2023-01-08 12:14:29 +0530

    edited

--- assignment final/19.py
+++ assignment final/19.py
@@ -20,18 +20,18 @@ n2 = input("Student 2 name: ")
 Nos1 = [95, 80, 90]
 Nos2 = [70, 99, 63]
 
+D = {"Student_1 name": n1, "Student_1 marks": Nos1, "Student_2 name": n2, "Student_2 marks": Nos2}
+print(D)
+
 sum1, sum2 = 0, 0
-for ele in Nos1:
+for ele in D["Student_1 marks"]:
     sum1 += ele
 
-for ele in Nos2:
+for ele in D["Student_2 marks"]:
     sum2 += ele
 
-avg1 = sum1 / len(Nos1)
-avg2 = sum2 / len(Nos2)
+avg1 = sum1 / len(D["Student_1 marks"])
+avg2 = sum2 / len(D["Student_2 marks"])
 
-D = dict([(n1, Nos1), (n2, Nos2)])
-print(D)
-# Dict = { "Student_1 Name": n1, " Studen_1 Numbers": Nos1, "Student_2 Name": n2, "Student_2 Numbers": Nos2}
-print("Student_1 name is: ", n1, ". His average marks is: ", avg1)
-print("Student_2 name is: ", n2, ". His average marks is: ", avg2)
+print("Student_1 name is: ", D["Student_1 name"], ". His average marks is: ", avg1)
+print("Student_2 name is: ", D["Student_2 name"], ". His average marks is: ", avg2)
